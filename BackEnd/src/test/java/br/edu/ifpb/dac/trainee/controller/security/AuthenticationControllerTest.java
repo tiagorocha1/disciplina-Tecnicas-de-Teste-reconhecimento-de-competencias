10e2719293135310f40fbeb3166cf8b820026fd7
Tiago Rocha <tiagoferreira.info@gmail.com>
2022-04-24 22:39:36 -0300

    Refactoring (Pattern PageObject)

--- BackEnd/src/test/java/br/edu/ifpb/dac/trainee/controller/security/AuthenticationControllerTest.java
+++ BackEnd/src/test/java/br/edu/ifpb/dac/trainee/controller/security/AuthenticationControllerTest.java
@@ -2,71 +2,57 @@ package br.edu.ifpb.dac.trainee.controller.security;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthenticationControllerTest {
 
-	private static final String URL_BASE="http://localhost:8081/?#/";
-	private WebDriver browser;
+	private LoginPage loginPage;
 
 	@BeforeEach
 	private void beforeEach() {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-		this.browser = new ChromeDriver();
-		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		this.loginPage = new LoginPage();
 	}
-	
+
 	@AfterEach
 	private void afterEach() {
-		browser.quit();
+		this.loginPage.quit();
 	}
 
 	@Test
 	void loginWithValidData() {
 
-		browser.navigate().to(URL_BASE+"login");
+		loginPage.fillField("inputEmail", "admin@task");
+		loginPage.fillField("inputPassword", "123");
+		loginPage.clickField("submit");
 
-		browser.findElement(By.id("inputEmail")).sendKeys("admin@task");
-		browser.findElement(By.id("inputPassword")).sendKeys("123");
-		browser.findElement(By.id("submit")).click();
+		assertNotNull(loginPage.getElement("restrito"));
+		assertTrue(loginPage.contains("logout"));
 
-		assertNotNull(browser.findElement(By.id("restrito")));
-		assertTrue(browser.getPageSource().contains("logout") );
-	
 	}
-	
+
 	@Test
 	void loginWithInValidData() {
 
-		browser.navigate().to(URL_BASE+"login");
+		loginPage.fillField("inputEmail", "admin@task");
+		loginPage.fillField("inputPassword", "123456");
+		loginPage.clickField("submit");
 
-		browser.findElement(By.id("inputEmail")).sendKeys("admin@task");
-		browser.findElement(By.id("inputPassword")).sendKeys("123456");
-		browser.findElement(By.id("submit")).click();
-
-	    
-		assertNotNull(browser.findElement(By.id("submit")));
-		assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("restrito")));
-	
+		assertNotNull(loginPage.getElement("submit"));
+		assertNull(loginPage.getElement("restrito"));
 
 	}
-	
+
 	@Test
 	void restrictedUrl() {
-		browser.navigate().to(URL_BASE+"tasks");
-		assertNotNull(browser.findElement(By.id("submit")));
-		assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("restrito")));
-		
+		loginPage.to("tasks");
+		assertNotNull(loginPage.getElement("submit"));
+		assertNull(loginPage.getElement("restrito"));
+
 	}
 
 }
---
+++ BackEnd/src/test/java/br/edu/ifpb/dac/trainee/controller/security/LoginPage.java
@@ -0,0 +1,58 @@
+package br.edu.ifpb.dac.trainee.controller.security;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	private static final String URL_BASE = "http://localhost:8081/?#/";
+	private WebDriver browser;
+
+	public LoginPage() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		this.browser = new ChromeDriver();
+		browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		this.to("login");
+	}
+
+	public void quit() {
+		browser.quit();
+
+	}
+
+	public void to(String page) {
+		browser.navigate().to(URL_BASE + page);
+	}
+
+	public void fillField(String field, String value) {
+		browser.findElement(By.id(field)).sendKeys(value);
+
+	}
+
+	public void clickField(String field) {
+		browser.findElement(By.id(field)).click();
+	}
+
+	public String getElement(String field) {
+		try {
+			return browser.findElement(By.id(field)).getText();	
+		}catch (NoSuchElementException e) {
+			return null;
+		}
+		
+
+	}
+
+	public boolean contains(String field) {
+		return browser.getPageSource().contains(field);
+
+	}
+
+}
